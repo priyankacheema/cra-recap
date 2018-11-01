adf9a898549a382600ac3999db65975d0cf18318
Priyanka Cheema <pcheema@qca-sdc.com>
2018-11-01 14:16:15 -0500

    feat: add giphy service call

--- src/components/Giphy/index.test.js
+++ src/components/Giphy/index.test.js
@@ -2,11 +2,36 @@ import React from "react";
 import { shallow } from "enzyme";
 import { expect } from "code";
 import Giphy from "./";
+import Chance from "chance";
 
 describe("Given Giphy", () => {
-  it("should exist with a specifying class name", () => {
-    const component = shallow(<Giphy />);
+  //props is always an object
+  //overrides ={key:value} pair
+  function requiredProps(overrides = {}) {
+    return {
+      ...overrides
+    };
+  }
+  function renderComponent(props = requiredProps()) {
+    return shallow(<Giphy {...props} />);
+  }
 
-    expect(component.is("ul.giphy-list")).to.be.true();
+  it("should exist with a specifying class name", () => {
+    const component = renderComponent();
+    expect(component.is("section.giphy")).to.be.true();
+  });
+
+  it("should exist with a specifying class name", () => {
+    const component = renderComponent();
+    expect(component.find("ul.giphy-list").length).to.equal(1);
+  });
+
+  describe('When there are "giphyItems" props', () => {
+    it("should render that many GifItems", () => {
+      const chance = new Chance();
+      const gifs = [{ id: chance.string() }, { id: chance.string() }];
+      const component = renderComponent({ giphyItems: gifs });
+      expect(component.find("GiphyItem").length).to.equal(gifs.length);
+    });
   });
 });
